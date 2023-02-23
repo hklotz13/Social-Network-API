3c7d32d5e7c1c80a97b702e0e773a8f469f6ff4c
Harper Klotz <harperklotz@Harpers-MacBook-Air.local>
2023-02-23 14:06:31 -0500

    adding thought model

---
+++ models/thoughts.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reactions');
+const dateFormat = (date) => {
+    return date.toString();
+};
+
+const thoughtSchema = new Schema(
+    {
+        thoughtBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+  
+  const Thought = model('Thought', thoughtSchema);
+  
+  module.exports = Thought;
